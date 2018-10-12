5af8568b034fc91c4bfee3bdf7d0f2b7b6b273ea
Outplayd <Outplayd@users.noreply.github.com>
2018-10-12 13:01:13 +0200

    Project D update 0

---
+++ Project - D (experimental)/Assets/code/entities/entity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Entity : MonoBehaviour {
+
+    public float speed = 5;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ Project - D (experimental)/Assets/code/entities/player/player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : Entity {
+
+
+
+    void Start() {
+
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.up * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.down * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * speed * Time.deltaTime;
+
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f ) // forward
+        {
+            speed += 1;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            speed -= 1; 
+        }
+    }
+}
